288392141a16cf72a84b31930b69f4ef65ac2d8b
eskorobogatov <egod09080908@yandex.ru>
2025-04-09 17:51:27 +0300

    very minor improvement in pure pursuit

--- solution_bachelor/scripts/pure_pursuit.py
+++ solution_bachelor/scripts/pure_pursuit.py
@@ -15,5 +15,9 @@ class PurePursuitController():
         self.wheelbase = 4.3
 
     def get_angle(self, cur_heading, next_point):
+
+        vec_len = np.linalg.norm(next_point)
         heading_angle = angle_from_vec_to_vec(cur_heading, next_point)
-        return np.arctan((2 * self.wheelbase * sin(heading_angle)) / self.lookahead)
+        return np.arctan(
+            (2 * self.wheelbase * sin(heading_angle)) / vec_len
+        )
